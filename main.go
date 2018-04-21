60c67af029a10836e70c1ef80d927f8f0202cd10
David López <dlopbec@gmail.com>
2018-04-21 11:19:43 +0200

    First interface created

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+//No hacen falta extends
+//Cualquier custom type que contenga una func
+//Que se llame getGreeeting() y devuelva un string
+//Se convierte automáticamente al tipo "bot"
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	//Very custom logic for generating english greeting
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	//Very custom logic for generating spanish greeting
+	return "Hola!"
 }
